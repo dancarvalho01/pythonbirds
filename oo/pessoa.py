8726342fa302ced237d78bd0cf3beb06234ce4f0
dancarvalho01 <daniloscarvalho1@hotmail.com>
2021-06-20 14:33:58 -0300

    Criado atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,5 @@
 class Pessoa:
+    olhos = 2
     def __init__(self,*filhos, nome=None, idade=35):
         self.idade = idade
         self. nome = nome
@@ -20,6 +21,12 @@ if __name__ == '__main__':
         print(filho.nome)
     joão.sobrenome = 'Ramalho'
     del joão.filhos
-    print(joão.__dict__)
+    joão.olhos = 1
+    del joão.olhos
     print(danilo.__dict__)
-
+    print(joão.__dict__)
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(joão.olhos)
+    print(danilo.olhos)
+    print(id(Pessoa.olhos), id(joão.olhos), id(danilo.olhos))
